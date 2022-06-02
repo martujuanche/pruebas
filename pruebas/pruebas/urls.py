5c77d093831cf040fd56c3c83aee1ba4be0451ae
martujuanchee <martinajuanche@gmail.com>
2022-06-02 19:40:24 -0300

    commit full

--- pruebas/pruebas/urls.py
+++ pruebas/pruebas/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from pruebas.views import saludo, index, fecha_actual, probando_template
+
 
 urlpatterns = [
+    path('', index, name = 'index'),
+    
     path('admin/', admin.site.urls),
+    path('saludo/<nombre>/', saludo, name = 'saludo'),
+
+    path('fecha_actual/', fecha_actual, name = 'fecha_actual'),
+    path('probando-template/', probando_template, name = 'probando_template'),
+
+    path('products/', include('products.urls'))
 ]
